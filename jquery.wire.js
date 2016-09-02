c3c7b8711f71e96b1af70e17c807a3a5f6885421
Mikael Karon <mikael@karon.se>
2016-09-02 10:23:36 +0200

    feat: callback should return instance not class I realized we can greatly simplify and generalize wire if we change factory to a callback that can async return a result. BREAKING CHANGE: Callback now has to return an instance not a module.

--- jquery.wire.js
+++ jquery.wire.js
@@ -7,40 +7,15 @@
     root["mu-jquery-wire/jquery.wire"] = factory(root.jQuery);
   }
 }(this, function($) {
-  var slice = Array.prototype.slice;
-
-  function construct(F, args) {
-    function C() {
-      return F.apply(this, args);
-    }
-
-    C.prototype = F.prototype;
-
-    return new C();
-  }
-
-  return $.fn.wire = function(attr, factory) {
-    var args = slice.call(arguments, 2);
+  return $.fn.wire = function(attr, callback) {
 
     return $.when.apply(null, this.map(function(index, element) {
-      var $element = $(this);
+      var $element = $(element);
 
       return $.when.apply(null, ($element.attr(attr) || "")
         .split(/\s+/)
         .map(function(module) {
-          return $.when(factory.call($element, module)).then(function(c) {
-            switch ($.type(c)) {
-              case "function":
-                return construct(c, [$element, module].concat(args));
-                break;
-
-              case "undefined":
-                break;
-
-              default:
-                throw new Error(c + " is not a supported type");
-            }
-          });
+          return $.when(callback.call($element, module));
         }));
     }));
   };
