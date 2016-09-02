42997c82798d2541f8ffd8e3351f4e0204e3a1ab
Mikael Karon <mikael@karon.se>
2016-09-02 10:53:50 +0200

    feat: follow standard jQuery praxsis for 'this' Standard praxis for jQuery plugins with callbacks looks to be to set 'this' to the element and not the jQuery blessed version of the element. BREAKING CHANGE: Users have to $(element) on their own in callback.

--- jquery.wire.js
+++ jquery.wire.js
@@ -8,14 +8,11 @@
   }
 }(this, function($) {
   return $.fn.wire = function(attr, callback) {
-
-    return $.when.apply(null, this.map(function(index, element) {
-      var $element = $(element);
-
-      return $.when.apply(null, ($element.attr(attr) || "")
+    return $.when.apply(null, this.map(function(i, element) {
+      return $.when.apply(null, ($(element).attr(attr) || "")
         .split(/\s+/)
-        .map(function(module) {
-          return $.when(callback.call($element, module));
+        .map(function(module, index) {
+          return $.when(callback.call(element, module, index));
         }));
     }));
   };
