82d5f56cc71bb5ceb91a1271e88eb5588fbb78e2
Mikael Karon <mikael@karon.se>
2016-09-02 09:48:33 +0200

    feat: remove .find from $.fn.wire and instead rely on 'this' BREAKING CHANGE: users will have to update their code to first select elements. A simple way to do this is by adding .find('[attr_goes_here]') right before the .wire call.

--- jquery.wire.js
+++ jquery.wire.js
@@ -22,28 +22,26 @@
   return $.fn.wire = function(attr, factory) {
     var args = slice.call(arguments, 2);
 
-    return $.when.apply(null, this
-      .find("[" + attr + "]")
-      .map(function(index, element) {
-        var $element = $(this);
+    return $.when.apply(null, this.map(function(index, element) {
+      var $element = $(this);
 
-        return $.when.apply(null, ($element.attr(attr) || "")
-          .split(/\s+/)
-          .map(function(module) {
-            return $.when(factory.call($element, module)).then(function(c) {
-              switch ($.type(c)) {
-                case "function":
-                  return construct(c, [$element, module].concat(args));
-                  break;
+      return $.when.apply(null, ($element.attr(attr) || "")
+        .split(/\s+/)
+        .map(function(module) {
+          return $.when(factory.call($element, module)).then(function(c) {
+            switch ($.type(c)) {
+              case "function":
+                return construct(c, [$element, module].concat(args));
+                break;
 
-                case "undefined":
-                  break;
+              case "undefined":
+                break;
 
-                default:
-                  throw new Error(c + " is not a supported type");
-              }
-            });
-          }));
-      }));
+              default:
+                throw new Error(c + " is not a supported type");
+            }
+          });
+        }));
+    }));
   };
 }));
