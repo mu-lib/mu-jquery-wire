ea410973377b165097cd0555b5482c82441cfa33
Mikael Karon <mikael@karon.se>
2016-09-12 12:25:52 +0200

    internal refactoring

--- jquery.wire.js
+++ jquery.wire.js
@@ -1,39 +1,42 @@
-(function(modules, root, factory) {
+(function (modules, root, factory) {
   if (typeof define === "function" && define.amd) {
     define(modules, factory);
   } else if (typeof module === "object" && module.exports) {
     module.exports = factory.apply(root, modules.map(require));
   } else {
-    root["mu-jquery-wire/jquery.wire"] = factory.apply(root, modules.map(function(m) {
+    root["mu-jquery-wire/jquery.wire"] = factory.apply(root, modules.map(function (m) {
       return {
         "jquery": root.jQuery
       }[m] || root[m];
     }));
   }
-})(["jquery"], this, function($) {
+})(["jquery"], this, function ($) {
   var slice = Array.prototype.slice;
-  var resolved = $.Deferred(function (dfd) {
-    dfd.resolve();
-  });
 
-  return function(input, callback) {
-    var args = slice.call(arguments, 2);
+  function collect() {
+    return slice.call(arguments);
+  }
+
+  return function (input, callback) {
     var self = this;
+    var args = slice.call(arguments, 2);
+    var resolved = $.Deferred(function (dfd) {
+      dfd.resolveWith(self);
+    });
 
     return self.length === 0
-      ? resolved :
-      $.when.apply(null, self.map(function(i, element) {
-        return $.when(input.apply(self, [element, i].concat(args))).then(function (_input) {
+      ? resolved
+      : $.when.apply(null, self.map(function (i, element) {
+        var $element = $(element);
+        return $.when($.isFunction(input) ? input.apply(self, [element = $element, i].concat(args)) : input).then(function (_input) {
           return _input === undefined || _input.length === 0
             ? resolved
-            : $.when.apply(null, $.map($.isArray(_input) ? _input : [_input], function(output, index) {
-              return $.when(callback.call(self, element, index, output)).then(function(result) {
-                return arguments.length > 1
-                  ? slice.call(arguments)
-                  : result || output;
-              });;
-          }));
+            : $.when.apply(null, $.map($.isArray(_input) ? _input : [_input], function (output, index) {
+              return $.when(callback.call(self, $element, index, output)).then(function (result) {
+                return arguments.length > 1 ? slice.call(arguments) : result || output;
+              });
+            })).then(collect);
         });
-    }));
+      })).then(collect);
   }
 });
