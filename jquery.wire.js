e3183cfc814d6ef637ccfc92c03730d5a14496a6
Mikael Karon <mikael@karon.se>
2017-06-16 03:31:43 +0200

    fix: UMD fixes
    Fix UMD wrapper to for static analysis

--- jquery.wire.js
+++ jquery.wire.js
@@ -1,12 +1,12 @@
-(function (modules, root, factory) {
+(function (root, factory) {
   if (typeof define === "function" && define.amd) {
-    define(modules, factory);
+    define([], factory);
   } else if (typeof module === "object" && module.exports) {
-    module.exports = factory.call(root);
+    module.exports = factory();
   } else {
-    root["mu-jquery-wire/jquery.wire"] = factory.call(root);
+    root["mu-jquery-wire/jquery.wire"] = factory();
   }
-})([], this, function () {
+})(this, function () {
   var slice = Array.prototype.slice;
 
   function collect() {
